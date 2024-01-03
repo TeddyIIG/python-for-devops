92aa92194ca95d8530ec6ffdc26c90a1229a08e3
Shehanka Fernando <shehankafernando@gmail.com>
2024-01-04 02:30:03 +0530

    Completed Day 01

--- Day-01/02-hello-world.py
+++ Day-01/02-hello-world.py
@@ -1,1 +1,7 @@
-print("Hello, World!")
+import requests, json
+
+
+headers = {"Accept":"application/vnd.github+json","Authorization":"Bearer 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","X-GitHub-Api-Version":"2022-11-28"}
+response = requests.get("https://api.github.com/repos/TeddyIIG/Portfolio/dependabot/alerts",headers=headers)
+json_response = json.loads(response.text)
+print(json_response)
